89757e700c895eb156bf945c793d56d1c05acc8d
西山 雄大 <nishiyamayudai@team-lab.com>
2021-03-29 21:51:06 +0900

    feat: Part 3

---
+++ models/genre.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: [3, 'Too few characters'],
+      max: [100, 'Too much characters']
+    }
+  }
+);
+
+// Virtual for genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/book/' + this._id;
+});
+
+// Export model
+module.exports = mongoose.model('Genre', GenreSchema);
+
